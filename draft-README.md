f326924fe9262b475e60ccbc3ed74f924eefb797
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-10-10 20:52:16 -0400

    Successfully rendered the map in the browser.

---
+++ draft-README.md
@@ -0,0 +1,45 @@
+# Neighborhood Map (React) Project
+
+## Overview
+
+Project Seven, the final project for Udacity's Front End Nanodegree ...
+
+
+### Working with Google's Maps API
+
+
+
+
+## Running The Project
+
+1. Clone or download this repository
+2. Install the project dependencies using `npm install`
+3. Run the development server with `npm start`
+4. In your browser, navigate to http://localhost:3000 to view a running instance of the application
+
+
+## Navigating the Neighborhood Map (React) App
+
+
+
+
+## Resources
+
+#### Course Resources
+
+- [Udacity Course: React Fundamentals](https://www.udacity.com/course/react-nanodegree--nd019) from Udacity's React Nanodegree
+
+- [Google Maps APIs by Google](https://www.udacity.com/course/google-maps-apis--ud864)
+
+- [Project Spec](https://review.udacity.com/#!/rubrics/1351/view)
+
+
+#### External Resources
+
+- [React Docs](https://reactjs.org/docs/getting-started.html?no-cache=1)
+
+- This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). You can find more information on how to perform common tasks [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
+
+
+#### Attributions
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.renderMap()
+    }
+
+    renderMap = () => {       
+        runScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBsq_UPJt6AeLwo4WtkTt1d9tZD7ozmhoU&callback=initMap")
+        window.initMap = this.initMap 
+    }
+
+    initMap = () => {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+
+    render() {
+        return (
+            <main>
+                <div id="map"></div>
+            </main>
+        )
+    }
+}
+
+
+function runScript(url) {
+	const body = window.document.querySelector('body');
+	const newScript = window.document.createElement('script');
+	newScript.src = url;
+	newScript.async = true;
+	newScript.defer = true;
+	body.insertAdjacentElement('beforeend', newScript);
 }
 
 export default App;
