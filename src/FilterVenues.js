66ef6bf95c9cff6097c58c8bafb42a8545d34abd
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-10-22 13:36:18 -0400

    Added coffeeVenues.json file to store data offline.

--- src/FilterVenues.js
+++ src/FilterVenues.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import escapeRegExp from 'escape-string-regexp';
+import escapeStrRegex from 'escape-string-regexp';
 
 class FilterVenues extends Component {
     constructor(props) {
@@ -33,9 +33,6 @@ class FilterVenues extends Component {
                 navListIsOpen: false
             });
         }
-
-        // manage list of venues displayed in list based on query state
-        this.controlDisplayedVenues(query);
     }
 
     toggleNavListDisplay = () => {
@@ -45,65 +42,10 @@ class FilterVenues extends Component {
         }));
     }
 
-    controlDisplayedVenues = (query) => {
-        // Provide custom object-context access variable for this scope
-        let bindToThis = this;
-        // initialize mutable container variables for holding filtered list of markers and venues
-        let collectedFilteredVenues;
-        let collectedFilteredMarkers;
-
-        // when query is truthy (has a value), apply RegExp pattern escapeRegExp for filtering venues
-        if (query) {
-            const match = new RegExp(escapeRegExp(query), 'i');
-
-            // // Add venue to the filtered array if its name matches the query
-            collectedFilteredVenues = this.state.filteredVenues.filter(venue =>
-                match.test(venue.venue.name)
-            );
-
-            // Add marker to the filtered array if its title matches the query
-            collectedFilteredMarkers = this.state.filteredMarkers.filter(marker =>
-                match.test(marker.title)
-            );
-
-            this.setState({
-                filteredVenues: collectedFilteredVenues,
-                filteredMarkers: collectedFilteredMarkers
-            });
-        } else {
-            this.setState({
-                filteredVenues: this.props.venuesList,
-                filteredMarkers: this.props.markers
-            });
-        }
-
-        // update visibility of markers on map according to the filteredMarkers state
-        this.props.markers.map(marker => marker.setVisible(false));
-        // Delay on JS event loop is necessary for effectively controlling marker visibility
-        // relative to user interactions
-        setTimeout(function () {
-            bindToThis.props.markers.map(marker =>
-                bindToThis.controlMarkerVisibility(marker))
-        }, 0)
-    }
-    
-    controlMarkerVisibility = (marker) => {
-        // matching markers from filteredMarkers array should be visible
-        this.state.filteredMarkers.map(filteredMarker => {
-            if (filteredMarker.id === marker.id) {
-                marker.setVisible(true)
-            }
-        });
-    }
-
     render () {
-        const query = this.state.query;
-
         return (
             <section>                
-                <form
-                    onSubmit={(event) => event.preventDefault()}
-                >                    
+                <form>                    
                     <button
                         onClick={() => this.toggleNavListDisplay()}
                     >
